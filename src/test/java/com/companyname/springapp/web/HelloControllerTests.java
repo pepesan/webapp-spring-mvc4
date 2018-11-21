25dd77eb2cf6c2664b53d9349d26008e59faf299
David Vaquero <pepesan@gmail.com>
2018-11-21 16:08:11 +0100

    actulaizado el pom para funcionar con tomcat8

--- src/test/java/com/companyname/springapp/web/HelloControllerTests.java
+++ src/test/java/com/companyname/springapp/web/HelloControllerTests.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Date;
+
 
 public class HelloControllerTests {
 
@@ -12,9 +14,9 @@ public class HelloControllerTests {
     public void testHandleRequestView() throws Exception{
         HelloController controller = new HelloController();
         ModelAndView modelAndView = controller.handleRequest(null, null);
-        assertEquals("WEB-INF/views/hello.jsp", modelAndView.getViewName());
+        assertEquals("hello.jsp", modelAndView.getViewName());
         assertNotNull(modelAndView.getModel());
-        String nowValue = (String) modelAndView.getModel().get("now");
+        Date nowValue = (Date) modelAndView.getModel().get("fecha");
         assertNotNull(nowValue);
 
     }
